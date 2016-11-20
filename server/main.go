8e4dca773072d1763884f8a8034f17257c25870c
Cyrus Hanlon <cyrushanlon@hotmail.com>
2016-11-20 20:21:06 +0000

    Allowed the server to serve multiple clients properly

--- server/main.go
+++ server/main.go
@@ -10,6 +10,7 @@ import (
 
 var (
 	ClientIPs = []string{}
+	wg sync.WaitGroup
 )
 
 func main() {
@@ -39,51 +40,57 @@ func main() {
 					break
 				}
 			}
-
 			if !in {
 				ClientIPs = append(ClientIPs, newAdd)
 			}
 
-			//serve the connection
-			for {
-				buffer := make([]byte, 4096)
-				n, err := conn.Read(buffer)
-				if err != nil {
-					log.Println(err)
-					break
-				}
-
-				if n == 0 {
-					continue
-				} else {
-
-					log.Println("Setting Clipboard to", string(buffer))
-
-					var wg2 sync.WaitGroup
-
-					wg2.Add(len(ClientIPs))
-					for _, v := range(ClientIPs) {
-						go func() {
-							outconn, err := net.Dial("tcp4", v+":6264")
-							if err != nil {
-								log.Println(err)
-								wg2.Done()
-								return
-							}
-
-							n, err := outconn.Write(buffer)
-							if err != nil {
-								log.Println("n:", n, err)
-							}
-							outconn.Close()
-							wg2.Done()
-						}()
-					}
-
-					wg2.Wait()
-				}
-			}
-			time.Sleep(1 * time.Second)
+			wg.Add(1)
+			log.Println("serving", newAdd)
+			go ServeConnection(conn)
 		}
 	}
+}
+
+func ServeConnection(conn net.Conn) {
+	//serve the connection
+	for {
+		buffer := make([]byte, 4096)
+		n, err := conn.Read(buffer)
+		if err != nil {
+			log.Println(err)
+			wg.Done()
+			break
+		}
+
+		if n == 0 {
+			continue
+		} else {
+
+			log.Println("Setting Clipboard to", string(buffer))
+
+			var wg2 sync.WaitGroup
+
+			wg2.Add(len(ClientIPs))
+			for _, v := range(ClientIPs) {
+				go func() {
+					outconn, err := net.Dial("tcp4", v+":6264")
+					if err != nil {
+						log.Println(err)
+						wg2.Done()
+						return
+					}
+
+					n, err := outconn.Write(buffer)
+					if err != nil {
+						log.Println("n:", n, err)
+					}
+					outconn.Close()
+					wg2.Done()
+				}()
+			}
+
+			wg2.Wait()
+		}
+	}
+	wg.Done()
 }
