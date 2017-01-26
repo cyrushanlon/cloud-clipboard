5e149d4861d759675685058671d18442cb900640
Cyrus <cyrushanlon@hotmail.com>
2017-01-26 22:42:35 +0000

    Added client registration and cleaned up main.go

--- client/cmd/main.go
+++ client/cmd/main.go
@@ -1,121 +1,112 @@
 package main
 
 import (
+	"github.com/atotto/clipboard"
 	"github.com/cyrushanlon/cloud-clipboard/client"
+	"io"
+	"log"
+	"net"
+	"time"
 )
 
 var (
 	ServerIP = "192.168.1.236:6263"
+	cb       = client.CurrentClipboard{}
 )
 
-func main() {
-	go client.LookForClients()
-	client.ListenForClients()
-}
+func receiveClipboard() {
+	ln, err := net.Listen("tcp4", ":6264")
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Listening to :6264")
 
-/*
-func main() {
+	for {
+		log.Println("Waiting for a connection...")
 
-	var wg sync.WaitGroup
-
-	cb := client.CurrentClipboard{}
-
-	wg.Add(2)
-
-	//serve clipboard in
-	go func() {
-
-		ln, err := net.Listen("tcp4", ":6264")
+		conn, err := ln.Accept()
 		if err != nil {
-			log.Fatal(err)
+			if err != io.EOF {
+				log.Println(err)
+			}
+			continue
 		}
-		log.Println("Listening to :6264")
+
+		log.Println("got a connection!")
 
 		for {
-			log.Println("Waiting for a connection...")
+			//blocking read
+			buffer := make([]byte, 20000)
+			buffSlice := []byte{}
+			_, err := conn.Read(buffer)
 
-			conn, err := ln.Accept()
 			if err != nil {
 				if err != io.EOF {
-					log.Println( err)
-				}
-				continue
-			}
-
-			log.Println("got a connection!")
-
-			for {
-				//blocking read
-				buffer := make([]byte, 20000)
-				buffSlice := []byte{}
-				_, err := conn.Read(buffer)
-
-				if err != nil {
-					if err != io.EOF {
-						log.Println(err)
-					}
-					break
-				}
-
-				for k, v := range buffer {
-					if v == 0 {
-						buffSlice = buffer[:k]
-						break
-					}
-				}
-
-				if len(buffSlice) == 0 {
-					break
-				} else {
-					log.Println("Setting Clipboard to", string(buffSlice))
-
-					clipboard.WriteAll(string(buffSlice))
-					cb.SetText(string(buffSlice))
-
-					time.Sleep(1 * time.Second)
-				}
-
-			}
-		}
-
-		wg.Done()
-	}()
-
-	//serve clipboard out
-	go func() {
-
-		for {
-
-			conn, err := net.Dial("tcp", ServerIP)
-			if err != nil {
-				log.Println(err)
-				time.Sleep(1*time.Second)
-				continue
-			}
-
-			for {
-				ReadClipBoard, err := clipboard.ReadAll()
-				if err != nil {
 					log.Println(err)
-					time.Sleep(1*time.Second)
-					continue
 				}
+				break
+			}
 
-				if ReadClipBoard != cb.GetText() {
-
-					log.Println("Sending Clipboard")
-					cb.SetText(ReadClipBoard)
-					conn.Write([]byte(ReadClipBoard))
+			for k, v := range buffer {
+				if v == 0 {
+					buffSlice = buffer[:k]
+					break
 				}
+			}
+
+			if len(buffSlice) == 0 {
+				break
+			} else {
+				log.Println("Setting Clipboard to", string(buffSlice))
+
+				clipboard.WriteAll(string(buffSlice))
+				cb.SetText(string(buffSlice))
 
 				time.Sleep(1 * time.Second)
 			}
+
+		}
+	}
+}
+
+func serveClipboard() {
+	for {
+		conn, err := net.Dial("tcp", ServerIP)
+		if err != nil {
+			log.Println(err)
+			time.Sleep(1 * time.Second)
+			continue
 		}
 
-		wg.Done()
-	}()
+		for {
+			ReadClipBoard, err := clipboard.ReadAll()
+			if err != nil {
+				log.Println(err)
+				time.Sleep(1 * time.Second)
+				continue
+			}
 
-	wg.Wait()
+			if ReadClipBoard != cb.GetText() {
 
+				log.Println("Sending Clipboard")
+				cb.SetText(ReadClipBoard)
+				conn.Write([]byte(ReadClipBoard))
+			}
+
+			time.Sleep(1 * time.Second)
+		}
+	}
 }
-*/
+
+func main() {
+
+	//send multicasts
+	go client.LookForClients()
+	//get multicasts
+	go client.ListenForClients()
+
+	//serve clipboard in
+	go receiveClipboard()
+	//serve clipboard out
+	go serveClipboard()
+}
--- client/utils.go
+++ client/utils.go
@@ -1,20 +1,50 @@
 package client
 
 import (
-	"net"
-	"log"
 	"encoding/hex"
+	"log"
+	"net"
 	"time"
 )
 
 const (
-	address = "232.49.101.200:6964"
+	address       = "232.49.101.200:6964"
 	maxReadBuffer = 8192
 )
 
+//holds a list of active clients
+var ClientList []string
+
+func StringArrayContains(list []string, target string) bool {
+	for i := 0; i < len(list); i++ {
+		if list[i] == target {
+			return true
+		}
+	}
+	return false
+}
+
+func StringArrayRemove(list *[]string, target string) {
+	for i := 0; i < len(*list); i++ {
+		if (*list)[i] == target {
+			l := append((*list)[:i], (*list)[i+1:]...)
+			list = &l
+			return
+		}
+	}
+}
+
 func msgHandler(src *net.UDPAddr, n int, b []byte) {
-	log.Println(n, "bytes read from", src)
-	log.Println(hex.Dump(b[:n]))
+	//log.Println(n, "bytes read from", src)
+	//log.Println()
+	body := hex.Dump(b[:n])
+	if body == "remove" {
+		StringArrayRemove(&ClientList, src.String())
+	} else if body == "add" {
+		if !StringArrayContains(ClientList, src.String()) { //add if it isnt already in
+			ClientList = append(ClientList, src.String())
+		}
+	}
 }
 
 func ListenForClients() {
@@ -22,14 +52,16 @@ func ListenForClients() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	listener, err := net.ListenMulticastUDP("udp", nil, addr)
+	c, err := net.ListenMulticastUDP("udp", nil, addr)
 	if err != nil {
 		log.Fatal(err)
 	}
-	listener.SetReadBuffer(maxReadBuffer)
+	c.SetReadBuffer(maxReadBuffer)
+
+	log.Println("Looking for clients.")
 	for {
 		b := make([]byte, maxReadBuffer)
-		n, src, err := listener.ReadFromUDP(b)
+		n, src, err := c.ReadFromUDP(b)
 		if err != nil {
 			log.Fatal("ReadFromUDP failed:", err)
 		}
@@ -43,8 +75,11 @@ func LookForClients() {
 		log.Fatal(err)
 	}
 	c, err := net.DialUDP("udp", nil, addr)
+
+	log.Println("Looking for servers.")
 	for {
-		c.Write([]byte("hello, world\n"))
+		log.Println("LFC - Sending Ping")
+		c.Write([]byte("add"))
 		time.Sleep(1 * time.Second)
 	}
 }
